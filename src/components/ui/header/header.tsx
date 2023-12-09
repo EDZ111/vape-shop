1256247d478907accf99888422a10cf5ae5bd206
EDZ111 <edoardo.cre@gmail.com>
2023-12-09 09:53:44 +0100

    first commit

---
+++ src/components/ui/header/header.tsx
@@ -0,0 +1,13 @@
+import { component$ } from "@builder.io/qwik";
+
+
+export default component$(() => {
+  
+  return (
+    <header>
+      <nav>
+        <div class="bg-yellow-500">header</div>
+      </nav>
+    </header>
+  );
+});
